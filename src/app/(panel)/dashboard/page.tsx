f3d4ca3b4d24bd5410908214a5cd652c76777bdb
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-04-23 14:03:37 -0300

    Bloqueando acesso a paginas

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,6 +1,13 @@
+import getSession from '@/lib/getSession'
+import { redirect } from 'next/navigation'
 
+export default async function Dashboard() {
+    const session = await getSession();
 
-export default function Dashboard() {
+    if(!session) {
+        redirect("/");
+    }
+
     return(
         <div>
             <h1>Página Dashboard</h1>
