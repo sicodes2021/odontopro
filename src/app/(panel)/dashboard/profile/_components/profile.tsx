e34dfc96949793eee61af0ad6433f48254140f1b
Santunis Oliveira <santunis.oliveira@gmail.com>
2025-04-24 09:47:45 -0300

    Gerando horários para o dialog

--- src/app/(panel)/dashboard/profile/_components/profile.tsx
+++ src/app/(panel)/dashboard/profile/_components/profile.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from 'react'
 import { useProfileForm } from './profile-form'
 import {
     Card,
@@ -37,6 +38,7 @@ import {
 
 import { Button } from '@/components/ui/button'
 import { ArrowRight } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
 import imgTest from '../../../../../../public/foto1.png'
 
@@ -44,7 +46,29 @@ import imgTest from '../../../../../../public/foto1.png'
 
 export function ProfileContent() {
 
+    const [selectedHours, setSelectedHours] = useState<string[]>([])
+
     const form = useProfileForm();
+
+    function generateTimeSlots(): string[] {
+        const hours: string[] = [];
+
+        for(let i = 8; i <= 24; i++) {
+            for(let j = 0; j < 2; j++) {
+                const hour = i.toString().padStart(2, "0")
+                const minute = (j * 30).toString().padStart(2, "0")
+                hours.push(`${hour}:${minute}`)
+            }
+        }
+
+        return hours;
+    }
+
+    const hours = generateTimeSlots();
+
+    function toggleHour(hour: string) {
+        setSelectedHours((prev) => prev.includes(hour) ? prev.filter(h => h !== hour) : [...prev, hour].sort())
+    }
 
     return (
         <div className='mx-auto'>
@@ -173,11 +197,20 @@ export function ProfileContent() {
                                             </DialogHeader>
 
                                             <section className='py-4'>
-                                                <p className='text-sm text-muted-foreground'>
+                                                <p className='text-sm text-muted-foreground mb-2'>
                                                     Clique nos horários abaixo para marcar ou desmarcar:
                                                 </p>
-                                                <div>
-                                                    ...
+                                                <div className='grid grid-cols-5 gap-2'>
+                                                    {hours.map((hour) => (
+                                                        <Button 
+                                                            key={hour}
+                                                            variant="outline"
+                                                            className={cn('h-10', selectedHours.includes(hour) && 'border-2 border-emerald-500 text-primary')}
+                                                            onClick={ () => toggleHour(hour) }
+                                                        >
+                                                            {hour}
+                                                        </Button>
+                                                    ))}
                                                 </div>
                                             </section>
                                         </DialogContent>
